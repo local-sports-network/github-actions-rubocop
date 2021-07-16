705f52fa9ce4de4f1748c19b705c090e7d47ae95
Ben Bean <bbean86@gmail.com>
2021-07-15 22:09:08 -0500

    Updated Rubocop versions again

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gem install rubocop -v 0.93.1
-gem install rubocop-rspec -v 1.44.1
-gem install rubocop-performance -v 1.10.2
-gem install rubocop-rails -v 2.9.1
+gem install rubocop -v 1.18.3
+gem install rubocop-rspec -v 2.4.0
+gem install rubocop-performance -v 1.11.4
+gem install rubocop-rails -v 2.11.3
 
 ruby /action/lib/index.rb
